616fd6918f904ef4e81e7ecdfd305fb9170aefea
meijiao <meijiao@gmail.com>
2017-01-16 17:40:05 -0800

    add my own test cases

--- index.js
+++ index.js
@@ -19,78 +19,53 @@ const Decode = require('./decode.js');
   function testArt (art) {
     var myEncoding = new Encode(art);
     var code = myEncoding.encode();
-    //console.log(code, 'code');
     var myDecoding = new Decode(code);
     var decode = myDecoding.decode();
     console.log(decode, 'decode');
     var results = (art === decode);
-    results ? console.log('successful encoding') : console.log('failed encoding');
+    results ? console.log('successful encoding of sample art') : console.log('failed encoding of sample art');
     return results
   }
 }('./data.txt'));
 
 
 // RUN TESTS ON STRINGS
-var test0 = 'aaaaa334'
-var test = 'aaaabb5'
-
-const basic = ['a', 'aa', 'aaa', 'aaaa']
-const basic_decode = ['a', 'aa', 'aa2', 'aa3']
-const whiteSpace = ['       ']; // 7 spaces ==> '  6'
+const basic = ['a', 'aa', 'aaa', 'aaaa', '       '];
+const basic_decode = ['a', 'aa', 'aa2', 'aa3', 'aa6']
 
 // more than 10 duplicates
 const moreThan10 = ['aaaaaaaaaaaa', 'aaaaaaaaaaaaaa']; //12a and 14a
 const moreThan10_decode = ['aa9aa', 'aa9aa3' ];
 
 // Cases where input contains numbers and they do not indicate duplicates
-const numbers = ['5566aaaaaaaaabc', 'aaaaaaa6bb2', 'aaaa3' ] //back to back, count equals next
-const numbers_decode = ['55|66aa8bc', 'aa6|6bb|2', 'aa3|3']
+const numbers = ['5566aaaaaaaaabc', 'aaaaaaa6bb2', 'aaaa3', 'aaaaa334', 'aaaabb5' ] //back to back, count equals next
+const numbers_decode = ['55|66aa8bc', 'aa6|6bb|2', 'aa3|3', 'aa4|33|4', 'aa3bb|5']
 
 // Bloated output ==> should return original
+const bloat = ['5566']; 
+const bloat_decode = ['5566'] //instead of "55|66" as it is longer
 
-var runTests = function (tests, solutions) {
+// Encode, then decode. Check that decoded art is the same as input
+var runTests = function (tests) {
   var bool = true;
+
   tests.forEach(function(test, index) {
     var myEncoding = new Encode(test);
     var code = myEncoding.encode();
-    if (code !== solutions[index]) {
+    var myDecoding = new Decode(code);
+    var decode = myDecoding.decode();
+
+    if (tests[index] !== decode) {
       console.log(`This test case failed: ${test}`);
       bool = false;
     }
   });
+
   bool ? console.log('all tests passed') : console.log('some tests failed, try again')
   return bool;
 }
 
-var runEncode = function (tests) {
-  var codes = [];
-  tests.forEach(function(test, index) {
-    var myEncoding = new Encode(test);
-    var code = myEncoding.encode();
-    codes.push(code);
-  });
-  return codes;
-}
+runTests(basic, basic_decode);  // ==> all tests passes
+runTests(moreThan10, moreThan10_decode); // ==> all tests passed
+runTests(numbers, numbers_decode); // ==> all tests passed
 
-//console.log(runEncode(whiteSpace));  // ==> all tests passes
-//runTests(basic, basic_decode);  // ==> all tests passes
-//runTests(moreThan10, moreThan10_decode); // ==> all tests passed
-//runTests(numbers, numbers_decode); // ==> all tests passed
-//console.log(runEncode (numbers));
-//var decode = myEncoding.decode(code);
-// console.log(decode, 'decode');
-
-var runDecode = function (tests) {
-  var decodes = [];
-  tests.forEach(function(test, index) {
-    var myDecoding = new Decode();
-    var decode = myDecoding.decode(test);
-    decodes.push(decode);
-  });
-  console.log(decodes);
-  return decodes;
-}
-
-// runDecode(['a', 'aa', 'aa2', 'aa3']);
-// runDecode(['aa9aa', 'aa9aa3']);
-// runDecode(['55|66aa8bc', 'aa6|6bb|2', 'aa3|3']);
