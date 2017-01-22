9db087dfda2fc889c330b21ce979297a901d4de6
meijiao <meijiao@gmail.com>
2017-01-21 21:50:39 -0800

    add written response

--- decode.js
+++ decode.js
@@ -1,41 +1,40 @@
 var Decode = function (string) {
-
   this.code = string;
+}
 
-  Decode.prototype.decode = function () {
-    var decode = '';
-    var printRepeats = false;
-    var copies = 0;
-    var parent = ''
-    
-    for (var i = 0; i < this.code.length; i++) {
-      // if you find an escape character before a number
-      if (this.code[i] === '|' && parseInt(this.code[i+1])) {
-        continue;
-      }
-      if (printRepeats) {
-        while (copies > 2) {
-          decode += parent;
-          copies --;
-        }
-        // reset
-        parent = '';
-        printRepeats = false;
-      } else {
-        decode += this.code[i];
-      }
-      // if you see 2 duplicates and the next item is a number: aa2
-      // toggle boolean to print duplicates for next round
-      if (!printRepeats && this.code[i] === this.code [i-1] && parseInt(this.code[i+1])) {
-        printRepeats = true;
-        parent = this.code[i];
-        copies = parseInt(this.code[i+1]);
-        decode += this.code[i];
-      }
-
+Decode.prototype.decode = function () {
+  var decode = '';
+  var printRepeats = false;
+  var copies = 0;
+  var parent = ''
+  
+  for (var i = 0; i < this.code.length; i++) {
+    // if you find an escape character before a number
+    if (this.code[i] === '|' && parseInt(this.code[i+1])) {
+      continue;
     }
-    return decode;
+    if (printRepeats) {
+      while (copies > 2) {
+        decode += parent;
+        copies --;
+      }
+      // reset
+      parent = '';
+      printRepeats = false;
+    } else {
+      decode += this.code[i];
+    }
+    // if you see 2 duplicates and the next item is a number: aa2
+    // toggle boolean to print duplicates for next round
+    if (!printRepeats && this.code[i] === this.code [i-1] && parseInt(this.code[i+1])) {
+      printRepeats = true;
+      parent = this.code[i];
+      copies = parseInt(this.code[i+1]);
+      decode += this.code[i];
+    }
+
   }
+  return decode;
 }
 
 module.exports = Decode;
---
+++ responses.md
@@ -0,0 +1,17 @@
+Hi,
+
+I'm sorry to hear about the error.
+
+Sadly, I wasn't able to pinpoint the error from looking at your code. It would be very helpful if you could give me the matching error messages:
+
+
+
+
+
+If you're still experiencing difficulties, could you please paste the entire error message in your reply? 
+
+Thanks for being patient,
+
+Mei
+
+
